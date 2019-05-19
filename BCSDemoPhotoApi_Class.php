1d3f0d13c458bc9c4f1758562ecedd873b026257
toby allen <tobyallen@toflidium.com>
2019-05-19 09:02:48 +0100

    Update PhotoAPI to better cover full photoapi

--- BCSDemoPhotoApi_Class.php
+++ BCSDemoPhotoApi_Class.php
@@ -9,25 +9,60 @@ class BCSDemoPhotoAPI extends BCSAPIClass
         $this->APIRootURL = $APIRootURL;
     }
 
+    // This function needs to be deleted, it is named wrong, it doesnt
+    // return images it returns gallereies
     public function  AllImages() {
+
+         $apipath =   '/all';
+
+         $res = $this->CallAPI($apipath);
+
+         $res['depreciated_message'] = 'calls to function AllImages() will be removed.  Does not return Images';
+         return $res;
+    }
+
+    // Return all Galleries from PhotoaPI.  Should be cached so fast.
+    public function  AllGalleries() {
 
          $apipath =   '/all';
          return $this->CallAPI($apipath);
     }
 
-    public function DemoPhotosInfo($DemoDate){
-        $apipath = '/gallery/{demodate}';
+
+    // Returns 1 random image from all galleries and all images.
+    public function RandomImage() {
+         $apipath =   '/images/random/';
+         return $this->CallAPI($apipath);
+    }
+
+
+    public function DemoPhotosInfo($DemoDate, $cached = true){
+        if ($cached){
+            $apipath = '/gallery/{demodate}';
+        } else {
+            $apipath = '/gallery/{demodate}/nocache';
+        }
+
         $Fields = ['{demodate}' => $DemoDate];
          return $this->CallAPI($apipath, $Fields);
+    }
 
+    public function PurgeCache() {
+         $apipath =   '/purgecache/';
+         return $this->CallAPI($apipath);
     }
 }
 
 /*
 
-
 $router->get('/all', 'photoController@AllPhotoInfo');
-$router->get('/images/', 'photoController@GalleryAlbum');
+$router->get('/all/{year}', 'photoController@YearPhotoInfo');
+//$router->get('/images/', 'photoController@GalleryAlbum');
+$router->get('/images/random/', 'photoController@GalleryImageRandom');
 $router->get('/gallery/{demodate}', 'photoController@GalleryAlbum');
+$router->get('/gallery/{demodate}/nocache', 'photoController@GalleryAlbum_noCache');
 $router->get('/gallery/{demodate}/html/{template}', 'photoController@HTMLGalleryAlbum');
+
+
+$router->get('/purgecache/', 'photoController@PurgeCache');
 */
--- tests/demophotos.php
+++ tests/demophotos.php
@@ -3,16 +3,19 @@ include_once('env.php');
 include_once('../BCSDemoPhotoApi_Class.php');
 
 
+
 $BCSDemoPHotos = new BCSDemoPhotoAPI($DemoPhotoAPIURL);
 
 echo '<PRE>';
-$AllImages = $BCSDemoPHotos->AllImages();
+$AllGalleries = $BCSDemoPHotos->AllGalleries();
+//print_r($AllGalleries);
+$RecentGallery = $AllGalleries['recent']['mostrecent'];
 
-echo "<li> AllImages Count: " . (count($AllImages['AllItems']) );
+echo "<li> AllGalleries Count: " . $AllGalleries['allitems_count'] ;
 
 $OneDemoDate = '20180612';
 $OneDemo = $BCSDemoPHotos->DemoPhotosInfo($OneDemoDate);
-
+//print_r($OneDemo);
 $OneDemoGallery =$OneDemo['gallery'];
 $Image_0 = $OneDemo['images'][0];
 echo "
@@ -20,7 +23,15 @@ echo "
 <LI> $OneDemoGallery[FolderName]
 <li> Image Count: $OneDemo[images_count]
 <li> Image 1 : <img src='$Image_0[src]' >
+<li> Most Recent Gallery : $RecentGallery[FolderName]
+";
 
+$RandomImage = $BCSDemoPHotos->RandomImage();
+//print_r($RandomImage);
+$img = $RandomImage['randomimage'];
+echo "
+<h3>Random Image </h3> 
+<img src=\"$img[src]\" >
 ";
 
 
