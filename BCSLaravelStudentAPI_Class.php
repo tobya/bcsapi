842cfca8f1a129c4d2de186403ae3cc2b0d8a135
Toby Allen <tobyallen@toflidium.com>
2021-01-27 20:08:04 +0000

    Subscriber Details (#12)

--- BCSLaravelStudentAPI_Class.php
+++ BCSLaravelStudentAPI_Class.php
@@ -3,27 +3,34 @@ require_once('BCSBaseApi_Class.php');
 
 class BCSLaravelStudentAPI extends BCSAPIClass
 {
-        function __construct($APIRootURL, $APIKEY) 
+        function __construct($APIRootURL, $APIKEY)
     {
         parent::__construct($APIRootURL, $APIKEY, false);
-        
+
     }
 
     Public function ActivateBooking($BookingID){
 
          $apipath =   '/api/v3/subscriptions/booking/{bookingid}/activate';
          $APIFields = ['{bookingid}' => $BookingID];
-         return $this->CallAPI($apipath, $APIFields); 
+         return $this->CallAPI($apipath, $APIFields);
     }
 
 
 
     public function SubscriptionCourseInfo($subscriptioncourseid){
-        
+
       //   https://bkoffice.azure.cookingisfun.ie/api/v3/subscriptions/subscribercourse/8
          $apipath =   '/api/v3/subscriptions/subscribercourse/{subscribercourseid}';
          $APIFields = ['{subscribercourseid}' => $subscriptioncourseid];
-         return $this->CallAPI($apipath, $APIFields); 
+         return $this->CallAPI($apipath, $APIFields);
     }
+
+    public function SubscriberDetails($IndividualID){
+        $apipath =  '/api/v3/subscriber/details/{individualid}';
+        $APIFields = ['{individualid}' => $IndividualID];
+        return $this->CallAPI($apipath,$APIFields);
+        }
+
 
 }
